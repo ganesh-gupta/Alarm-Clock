fd3cabbb7ddd1142e0e480757c5b177ab9055a3f
ganesh-gupta <guptaganesh890@gmail.com>
2023-08-31 11:53:07 +0530

    Add files via upload

---
+++ Alarm Clock/Step6/alarmClockApp/alarmClockApp.js
@@ -0,0 +1,68 @@
+import { LightningElement } from 'lwc';
+import AlarmClockAssets from '@salesforce/resourceUrl/AlarmClockAssets'
+export default class AlarmClockApp extends LightningElement {
+  clockImage = AlarmClockAssets+'/AlarmClockAssets/clock.png'
+  currentTime = ''
+  hours =[]
+  minutes = []
+  meridiems =['AM', 'PM']
+
+  hourSelected
+  minSelected
+  meridiemSelected
+
+  connectedCallback(){
+    this.createHoursOptions()
+    this.createMinutesOptions()
+    this.currentTimeHandler()
+  }
+  currentTimeHandler(){
+    setInterval(()=>{
+      let dateTime = new Date()
+      let hour = dateTime.getHours()//getHours() returns the hour (0 to 23) of a date.
+      let min = dateTime.getMinutes()
+      let sec = dateTime.getSeconds()
+      let ampm = "AM"
+      if(hour == 0){
+        hour = 12
+      } else if(hour >=12){
+        hour = hour - 12
+        ampm = "PM"
+      }
+      hour = hour<10 ? "0"+hour : hour
+      min = min<10 ? "0"+min : min
+      sec  = sec<10 ? "0"+sec : sec
+  
+      this.currentTime = `${hour}:${min}:${sec} ${ampm}`
+
+    }, 1000)
+  }
+
+  createHoursOptions(){
+    for(let i = 1; i<=12; i++){
+      let val = i<10 ? "0"+i : i
+      this.hours.push(val)
+    }
+  }
+  createMinutesOptions(){
+    for(let i = 0; i<=59; i++){
+      let val = i<10 ? "0"+i : i
+      this.minutes.push(val)
+    }
+  }
+
+  optionhandler(event){
+    const {label, value} = event.detail
+    if(label === "Hour(s)"){
+      this.hourSelected = value
+    } else if(label === "Minute(s)"){
+      this.minSelected = value
+    } else if(label === "AM/PM"){
+      this.meridiemSelected = value
+    } else {}
+
+    console.log(" this.hourSelected",  this.hourSelected)
+    console.log(" this.minSelected",  this.minSelected)
+    console.log(" this.meridiemSelected",  this.meridiemSelected)
+  }
+}
